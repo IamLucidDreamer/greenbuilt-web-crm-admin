1d4f673581c4f292d0d22b443259bca60403cf5d
Manas <shuklamanasofficial@gmail.com>
2022-07-20 12:36:52 +0530

    New BUild

--- src/appConfig/httpHelper.js
+++ src/appConfig/httpHelper.js
@@ -1,6 +1,6 @@
 const { default: Axios } = require("axios");
 const axios = Axios.create({
-  baseURL: "http://localhost:8002/api"
+  baseURL: "http://65.1.92.60:8002/api"
 });
 
 export default axios;
