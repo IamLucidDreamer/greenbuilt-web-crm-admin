762b975a64a153931e3bfdbd06814bd33894846c
Manas Shukla <78421038+IamLucidDreamer@users.noreply.github.com>
2024-03-29 15:59:23 +0530

    Update httpHelper.js

--- src/appConfig/httpHelper.js
+++ src/appConfig/httpHelper.js
@@ -1,6 +1,6 @@
 const { default: Axios } = require("axios");
 const axios = Axios.create({
-  baseURL: "http://35.154.146.75/api"
+  baseURL: "http://43.204.97.58:8002/api"
 });
 
 export default axios;
